4844bc92d522ba70a0d1810d7a590685c60e9e89
peroly <mernawatispeed08@gmail.com>
2026-02-20 17:13:54 +0700

    dummy

---
+++ src/main/java/id/ac/ui/cs/advprog/yomubackend/repository/KalkulasiRepository.java
@@ -0,0 +1,7 @@
+package id.ac.ui.cs.advprog.yomubackend.repository;
+
+import id.ac.ui.cs.advprog.yomubackend.model.Kalkulasi;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface KalkulasiRepository extends JpaRepository<Kalkulasi, Long> {
+}
